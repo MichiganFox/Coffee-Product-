4590d493d7c2fcef120ee8214a34c842169824af
Earle Fox <earle.fox312@gmail.com>
2023-02-10 08:51:02 -0500

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using Coffee.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Coffee.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        //public IActionResult result(string firstName, string lastName, string email, string password)
+        //{
+        //    string result = $"Thank you for your registration {firstName} {lastName}!";
+        //    return View("Registration", result);
+        //}
+        public IActionResult result(User c)
+        {
+            return View(c);
+        }
+
+        public IActionResult registration()
+        {
+            return View();
+        }
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace Coffee.Models
+{
+    public class User
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string emailAddress { get; set; }
+        public string password { get; set; }
+    }
+}
